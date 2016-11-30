6b2aaa5d9aedfbc8f1db7f40f3a0f37dcb2c2a27
Nathan Gaberel <nathang@theodo.co.uk>
2016-11-30 10:48:26 +0000

    Use browserHistory.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import { App, Notebook, Summary } from './components';
 
@@ -10,7 +10,7 @@ import './style/app.styl';
 import './style/hl.css';
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Summary} />
       <Route path="nb/:notebook" component={Notebook} />
