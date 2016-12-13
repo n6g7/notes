8899ed05d2480fc48d8656af24abdd929019fbad
Nathan Gaberel <nathang@theodo.co.uk>
2016-12-13 22:52:06 +0000

    Add secretary note.

--- notebooks/index.js
+++ notebooks/index.js
@@ -1,7 +1,9 @@
 import bertrand from './bertrand.ipynb';
+import secretary from './secretary.ipynb';
 import simulation from './simulation.ipynb';
 
 export default {
   bertrand,
+  secretary,
   simulation
 };
