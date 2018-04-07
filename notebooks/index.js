84e6f99a61db8da1e61f5340764526a3b2337fb0
Nathan Gaberel <nathang@theodo.co.uk>
2018-04-08 00:17:13 +0100

    Add star death notebook.

--- notebooks/index.js
+++ notebooks/index.js
@@ -2,5 +2,6 @@ export default {
   'accelerating-returns': () => import(/* webpackChunkName: "accelerating-returns" */ './accelerating-returns.ipynb'),
   'bertrand': () => import(/* webpackChunkName: "bertrand" */ './bertrand.ipynb'),
   'secretary': () => import(/* webpackChunkName: "secretary" */ './secretary.ipynb'),
-  'simulation': () => import(/* webpackChunkName: "simulation" */ './simulation.ipynb')
+  'simulation': () => import(/* webpackChunkName: "simulation" */ './simulation.ipynb'),
+  'star-death': () => import(/* webpackChunkName: "star-death" */ './star-death.ipynb')
 }
